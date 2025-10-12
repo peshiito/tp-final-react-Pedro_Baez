bd4cce613b94fac8f85e2d6731db9ac76dc8d2c2
peshiito <pgbaez0@gmail.com>
2025-10-11 21:43:07 -0300

    update y finish, falta el seccion de favoritos nomas, despues todo bien

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tp-final-react-Pedro_Baez">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
